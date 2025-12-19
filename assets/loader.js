d746d40cac06a5c4a952b828c535bfb9e70b3e93
Bhanupratap Biswas <63925690+BS-World@users.noreply.github.com>
2025-12-19 19:22:56 +0530

    Add files via upload

---
+++ assets/loader.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+  // Load Header
+  fetch("partials/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  // Load Footer
+  fetch("partials/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+
+});
